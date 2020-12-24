509dc8b4c5688b9440e2e3c7521e53b1748b363e
Kirill Pastukhov <Pastukhov475@icloud.com>
2020-12-24 17:56:54 +0300

    add linter

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
-import readlineSync from "readline-sync";
-export const readline = () => {
-  const name = readlineSync.question("May I have your name? ");
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello ${name}!`);
 };
