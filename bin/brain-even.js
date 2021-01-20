1ae4bbfdbbdebd84ace9a8aa3bc9978730b2814e
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-01-20 19:17:48 +0300

    fix bag

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,12 +7,12 @@ const getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 const isEven = (num) => (num % 2 === 0 ? "yes" : "no");
+
 const game = () => {
   console.log("Welcome to the Brain Games!");
-  const name = readLineSync.question("May i have your name? ");
-  console.log(
-    `Hello, ${name}\nAnswer "yes" if the number is even, otherwise answer "no".`
-  );
+  const name = readLineSync.question("May I have your name? ");
+  console.log(`Hello ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 1; i <= 3; i += 1) {
     let randomNum = getRandom(1, 100);
     console.log(`Question: ${randomNum}`);
@@ -21,9 +21,7 @@ const game = () => {
       console.log("Correct!");
     } else {
       console.log(
-        `${answer} is wrong answer ;(. Correct answer was ${isEven(
-          !randomNum
-        )}.`
+        `${answer} is wrong answer ;(. Correct answer was ${isEven(randomNum)}.`
       );
       return;
     }
