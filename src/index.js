ea76628e680b8fc7105eae6e68d7f32266a81b03
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-01-21 11:26:26 +0300

    fix linter bags

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
-import readLineSync from "readline-sync";
+import readLineSync from 'readline-sync';
 
-const getRandom = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+const getRandom = (begin, end) => {
+  const min = Math.ceil(begin);
+  const max = Math.floor(end);
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
 const game = () => {
-  console.log("Welcome to the Brain Games!");
-  const name = readLineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const name = readLineSync.question('May I have your name? ');
   console.log(`Hello ${name}!`);
-  console.log(""); //task
+  console.log('');// task
   for (let i = 1; i < 4; i += 1) {
     console.log(`Question: ${0}`);
-    const answer = readLineSync.question("Your answer: ");
+    const answer = readLineSync.question('Your answer: ');
     if (answer === 0) {
-      console.log("Correct!");
+      console.log('Correct!');
     } else {
       console.log(`${answer} is wrong answer ;(. Correct answer was ${0}.`);
       return;
