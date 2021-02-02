489e98f0f8bbfff9fda983c82e4fa7cccd7e9c67
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-02-02 16:42:00 +0300

    fix no-param-reassign

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-import { operation, calculateResult, statement, input, getRandomInput } from '../src/games/brain-calc.js';
+import { operation, calculateResult, statement, input, getRandomInput,
+} from '../src/games/brain-calc.js';
+
 import runGame from '../src/index.js';
 
 runGame(statement, operation, calculateResult, input, getRandomInput);
