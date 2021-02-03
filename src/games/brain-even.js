429561a24d769693f588041718d967d46a3ef5c2
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-02-03 08:30:32 +0300

    adapt common func for brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -17,8 +17,8 @@ import getRandomNumber from '../utils.js';
 };
 */
 export const input = getRandomNumber(1, 100);
-const isEven = (input) => (input % 2 === 0);
+const isEven = (inputData) => (inputData % 2 === 0);
 export const statement = 'Answer "yes" if the number is even, otherwise answer "no".';
 export const getRandomInput = () => getRandomNumber(1, 100);
-export const showInput = (input) => `${input}`;
-export const getResult = (input) => (isEven(input) ? 'yes' : 'no');
+export const showInput = (inputData) => `${inputData}`;
+export const getResult = (inputData) => (isEven(inputData) ? 'yes' : 'no');
