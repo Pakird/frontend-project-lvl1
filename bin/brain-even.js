e8ae5c7c3a16b84c3acbccedd98c6b52c2994f30
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-02-03 08:35:54 +0300

    adapt common func for brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 import runGame from '../src/index.js';
 
-import { statement, showInput, getResult, input, getRandomInput,
+import { statement,
+    showInput,
+    getResult,
+    input,
+    getRandomInput,
 } from '../src/games/brain-even.js';
 
 runGame(statement, showInput, getResult, input, getRandomInput);
