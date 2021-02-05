37b1a1ec95a6224778681cc315e6a78d9012f108
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-02-05 17:34:18 +0300

    unite functions in index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import readLineSync from 'readline-sync';
 
-export const greet = () => {
+export default (statement, question, result, input, getRandomInput) => {
+  const steps = 3;
   console.log('Welcome to the Brain Games!');
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
-};
-
-export const askQuestionTakeAnswer = (question) => {
-  console.log(` Question: ${question}`);
-  const answer = readLineSync.question('Your answer: ');
-  return answer;
-};
-
-export const steps = 3;
-
-export const executeGame = (answer, result) => answer === result || Number(answer) === result;
-
-export default (statement, question, result, input, getRandomInput) => {
-  const name = greet();
   let inputData = input;
   console.log(statement);
-  for (let i = 1; i <= steps; i += 1) {
-    const answer = askQuestionTakeAnswer(question(inputData));
+  for (let i = 0; i < steps; i += 1) {
+    console.log(` Question: ${question(inputData)}`);
+    const answer = readLineSync.question('Your answer: ');
 
-    const gameResult = executeGame(answer, result(inputData));
+    const gameResult = (answer === result(inputData) || Number(answer) === result(inputData));
     if (!gameResult) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result(inputData)}'.`);
       console.log(`Let's try again, ${name}!`);
